de078a47710d9648ea8f3c59ca300c7c248d457d
YasmynCH <yasmyn_3395@hotmail.com>
2022-05-05 13:38:31 -0600

    Se eliminan pruebas duplicadas y se unifican según el tipo de envio

--- tests/tests_envioTipo1_calculoEnvio.cpp
+++ tests/tests_envioTipo1_calculoEnvio.cpp
@@ -1,177 +1,178 @@
 #include <gtest/gtest.h>
 #include "../src/envioTipo1.h"
 
+using namespace std;
+
 namespace
 {
-    TEST(Test_EnvioTipo1_CalculoEnvio, Test_PrimeraClaseMayor0Menor3)
+    TEST(Test_EnvioTipo1_CalculoEnvio1, Test_PrimeraClaseMayor0Menor3)
     {
         /// AAA
 
         // Arrange - configurar el escenario
-        EnvioTipo1 envioTipo1;
-        // Act - ejecute la operación
-        int clasePorKm= 1;
-        int peso= 3;
-        int distancia= 4;
+        int clasePorKm = 1;
+        int peso = 3;
+        int distancia = 4;
 
-        float costoActual = envioTipo1.CalculoEnvioTipo1(peso, distancia, clasePorKm);
+        EnvioTipo1 envioTipo1(peso, distancia, clasePorKm);
+        // Act - ejecute la operación
+
+        float costoActual = envioTipo1.CalculoEnvio();
         float costoEsperado = 1.2;
 
         // Assert - valide los resultados
         EXPECT_FLOAT_EQ(costoEsperado, costoActual);
     }
 
-TEST(Test_EnvioTipo1_CalculoEnvioPrimeraClase, Test_PrimeraClaseMayor4Menor8)
+    TEST(Test_EnvioTipo1_CalculoEnvio2, Test_PrimeraClaseMayor4Menor8)
     {
         /// AAA
 
         // Arrange - configurar el escenario
-        EnvioTipo1 envioTipo1;
+        int clasePorKm = 1;
+        int peso = 5;
+        int distancia = 6;
+        EnvioTipo1 envioTipo1(peso, distancia, clasePorKm);
 
         // Act - ejecute la operación
-        int clasePorKm= 1;
-        int peso= 5;
-        int distancia= 6;
 
-        float costoActual = envioTipo1.CalculoEnvioTipo1(peso, distancia, clasePorKm);
+        float costoActual = envioTipo1.CalculoEnvio();
         float costoEsperado = 2.7;
 
         // Assert - valide los resultados
         EXPECT_FLOAT_EQ(costoEsperado, costoActual);
     }
 
-
-    TEST(Test_EnvioTipo1_CalculoEnvioPrimeraClase, Test_PrimeraClaseMayor9)
+    TEST(Test_EnvioTipo1_CalculoEnvio3, Test_PrimeraClaseMayor9)
     {
         /// AAA
 
         // Arrange - configurar el escenario
-        EnvioTipo1 envioTipo1;
+        int clasePorKm = 1;
+        int peso = 10;
+        int distancia = 15;
+        EnvioTipo1 envioTipo1(peso, distancia, clasePorKm);
 
         // Act - ejecute la operación
-        int clasePorKm= 1;
-        int peso= 10;
-        int distancia=15;
 
-        float costoActual = envioTipo1.CalculoEnvioTipo1(peso, distancia, clasePorKm);
+        float costoActual = envioTipo1.CalculoEnvio();
         float costoEsperado = 9;
 
         // Assert - valide los resultados
         EXPECT_FLOAT_EQ(costoEsperado, costoActual);
     }
-    
-    TEST(Test_EnvioTipo1_CalculoEnvioSegundaClase, Test_PrimeraClaseMayor0Menor3)
+    TEST(Test_EnvioTipo1_CalculoEnvio4, Test_PrimeraClaseMayor0Menor3)
     {
         /// AAA
 
         // Arrange - configurar el escenario
-        EnvioTipo1 envioTipo1;
+
+        int clasePorKm = 2;
+        int peso = 3;
+        int distancia = 3;
+        EnvioTipo1 envioTipo1(peso, distancia, clasePorKm);
 
         // Act - ejecute la operación
-        int clasePorKm= 2;
-        int peso= 3;
-        int distancia= 3;
 
-        float costoActual = envioTipo1.CalculoEnvioTipo1(peso, distancia, clasePorKm);
+        float costoActual = envioTipo1.CalculoEnvio();
         float costoEsperado = 0.084;
 
         // Assert - valide los resultados
         EXPECT_FLOAT_EQ(costoEsperado, costoActual);
     }
 
-
-    TEST(Test_EnvioTipo1_CalculoEnvioSegundaClase, Test_PrimeraClaseMayor4Menor8)
+    TEST(Test_EnvioTipo1_CalculoEnvio5, Test_PrimeraClaseMayor4Menor8)
     {
         /// AAA
 
         // Arrange - configurar el escenario
-        EnvioTipo1 envioTipo1;
+        int clasePorKm = 2;
+        int peso = 5;
+        int distancia = 9;
+        EnvioTipo1 envioTipo1(peso, distancia, clasePorKm);
 
         // Act - ejecute la operación
-        int clasePorKm= 2;
-        int peso= 5;
-        int distancia=9;
 
-        float costoActual = envioTipo1.CalculoEnvioTipo1(peso, distancia, clasePorKm);
+        float costoActual = envioTipo1.CalculoEnvio();
         float costoEsperado = 0.477;
 
         // Assert - valide los resultados
         EXPECT_FLOAT_EQ(costoEsperado, costoActual);
     }
 
-
-    TEST(Test_EnvioTipo1_CalculoEnvioSegundaClase, Test_PrimeraClaseMayor9)
+    TEST(Test_EnvioTipo1_CalculoEnvio6, Test_PrimeraClaseMayor9)
     {
         /// AAA
 
         // Arrange - configurar el escenario
-        EnvioTipo1 envioTipo1;
+        int clasePorKm = 2;
+        int peso = 10;
+        int distancia = 6;
+
+        EnvioTipo1 envioTipo1{peso, distancia, clasePorKm};
 
         // Act - ejecute la operación
-        int clasePorKm= 2;
-        int peso= 10;
-        int distancia= 6;
 
-        float costoActual = envioTipo1.CalculoEnvioTipo1(peso, distancia, clasePorKm);
+        float costoActual = envioTipo1.CalculoEnvio();
         float costoEsperado = 0.45;
 
         // Assert - valide los resultados
         EXPECT_FLOAT_EQ(costoEsperado, costoActual);
     }
 
-        TEST(Test_EnvioTipo1_CalculoEnvioTerceraClase, Test_PrimeraClaseMayor0Menor3)
+    TEST(Test_EnvioTipo1_CalculoEnvio7, Test_PrimeraClaseMayor0Menor3)
     {
         /// AAA
 
         // Arrange - configurar el escenario
-        EnvioTipo1 envioTipo1;
+
+        int clasePorKm = 3;
+        int peso = 3;
+        int distancia = 7;
+        EnvioTipo1 envioTipo1(peso, distancia, clasePorKm);
 
         // Act - ejecute la operación
-        int clasePorKm= 3;
-        int peso= 3;
-        int distancia= 7;
 
-        float costoActual = envioTipo1.CalculoEnvioTipo1(peso, distancia, clasePorKm);
+        float costoActual = envioTipo1.CalculoEnvio();
         float costoEsperado = 0.084;
 
         // Assert - valide los resultados
         EXPECT_FLOAT_EQ(costoEsperado, costoActual);
     }
 
-
-    TEST(Test_EnvioTipo1_CalculoEnvioTerceraClase, Test_PrimeraClaseMayor4Menor8)
+    TEST(Test_EnvioTipo1_CalculoEnvio8, Test_PrimeraClaseMayor4Menor8)
     {
         /// AAA
 
         // Arrange - configurar el escenario
-        EnvioTipo1 envioTipo1;
+        int clasePorKm = 3;
+        int peso = 5;
+        int distancia = 15;
+        EnvioTipo1 envioTipo1(peso, distancia, clasePorKm);
 
         // Act - ejecute la operación
-        int clasePorKm= 3;
-        int peso= 5;
-        int distancia=15;
 
-        float costoActual = envioTipo1.CalculoEnvioTipo1(peso, distancia, clasePorKm);
+        float costoActual = envioTipo1.CalculoEnvio();
         float costoEsperado = 0.18;
 
         // Assert - valide los resultados
         EXPECT_FLOAT_EQ(costoEsperado, costoActual);
     }
 
-
-    TEST(Test_EnvioTipo1_CalculoEnvioTerceraClase, Test_PrimeraClaseMayor9)
+    TEST(Test_EnvioTipo1_CalculoEnvio9, Test_PrimeraClaseMayor9)
     {
         /// AAA
 
         // Arrange - configurar el escenario
-        EnvioTipo1 envioTipo1;
+
+        int clasePorKm = 3;
+        int peso = 10;
+        int distancia = 25;
+        EnvioTipo1 envioTipo1(peso, distancia, clasePorKm);
 
         // Act - ejecute la operación
-        int clasePorKm= 3;
-        int peso= 10;
-        int distancia= 25;
 
-        float costoActual = envioTipo1.CalculoEnvioTipo1(peso, distancia, clasePorKm);
+        float costoActual = envioTipo1.CalculoEnvio();
         float costoEsperado = 0.3;
 
         // Assert - valide los resultados
--- tests/tests_envioTipo2_calculoEnvio.cpp
+++ tests/tests_envioTipo2_calculoEnvio.cpp
@@ -1,22 +1,63 @@
 #include <gtest/gtest.h>
 #include "../src/envioTipo2.h"
 
+using namespace std;
+
 namespace
 {
-    TEST(Test_EnvioTipo2_CalculoEnvioSegundaClase, Test_DistanciaMayor500)
+    TEST(Test_EnvioTipo2_CalculoEnvio, Test_DistanciaMayor500)
     {
         /// AAA
 
         // Arrange - configurar el escenario
-        EnvioTipo2 envioTipo2;
+
+        int distancia = 600;
+        int peso = 9;
+        int clasePorKm = 0;
+        EnvioTipo2 envioTipo2(peso, distancia, clasePorKm);
 
         // Act - ejecute la operación
-        int distancia= 600;
-        int peso= 9;
-        int clasePorKm=0;
 
-        float costoActual = envioTipo2.CalculoEnvioTipo2(peso, distancia, clasePorKm);
+        float costoActual = envioTipo2.CalculoEnvio();
         float costoEsperado = 50;
+
+        // Assert - valide los resultados
+        EXPECT_FLOAT_EQ(costoEsperado, costoActual);
+    }
+
+    TEST(Test_EnvioTipo2_CalculoEnvioSegundaClase, Test_PesoMayor10)
+    {
+        /// AAA
+
+        // Arrange - configurar el escenario
+        int distancia = 400;
+        int peso = 11;
+        int clasePorKm = 0;
+        EnvioTipo2 envioTipo2(peso, distancia, clasePorKm);
+
+        // Act - ejecute la operación
+
+        float costoActual = envioTipo2.CalculoEnvio();
+        float costoEsperado = 45;
+
+        // Assert - valide los resultados
+        EXPECT_FLOAT_EQ(costoEsperado, costoActual);
+    }
+
+    TEST(Test_EnvioTipo2_CalculoEnvioSClase, Test_DistanciaMayor500PesoMayor10)
+    {
+        /// AAA
+
+        // Arrange - configurar el escenario
+        int distancia = 900;
+        int peso = 15;
+        int clasePorKm = 0;
+        EnvioTipo2 envioTipo2(peso, distancia, clasePorKm);
+
+        // Act - ejecute la operación
+
+        float costoActual = envioTipo2.CalculoEnvio();
+        float costoEsperado = 60;
 
         // Assert - valide los resultados
         EXPECT_FLOAT_EQ(costoEsperado, costoActual);
